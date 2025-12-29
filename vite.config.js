789bdeed91e3e3621373c70f8e5392446a71fa5c
ArphyOMG <omk921001@gmail.com>
2025-12-29 22:46:10 +0900

    Configure Vite to disable minification and compression
    Set build options to prevent minification and compression.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
-    global: "window",
+    global: "window", // 아까 했던 global 변수 해결
+  },
+  build: {
+    minify: false, // ★★★ 여기가 핵심! "압축하지 마" (에러 해결)
+    terserOptions: {
+      compress: false,
+      mangle: false,
+    },
   },
 })
